66d69e89f6a638521d18b80b5bbdc9b79c90cae3
Anna_Victoria <Anna_Victoria@noreply.gitcode.com>
2025-06-06 02:02:17 +0800

    初始化：前三个测试

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,16 @@
 #include "bst.h"
+
+// Node类实现
+BST::Node::Node(int value, Node* left, Node* right)
+    : value(value), left(left), right(right) {}
+
+BST::Node::Node()
+    : value(0), left(nullptr), right(nullptr) {}
+
+BST::Node::Node(const Node& node)
+    : value(node.value), left(node.left), right(node.right) {}
+
+// BST类实现
+BST::Node*& BST::get_root() {
+    return root;
+}
